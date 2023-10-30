718e91826131fb1194cdf57566fbfcd12e5dde7d
aaaarvieeee <119615693+aaaarvieeee@users.noreply.github.com>
2023-10-29 19:03:14 -0600

    lol

---
+++ App.js
@@ -0,0 +1,68 @@
+/**
+ * My To Do List App
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet
+} from 'react-native';
+import ToDoForm from './ToDoForm';
+import ToDoList from './ToDoList';
+import { useState } from 'react';
+
+
+
+function App() {
+
+  
+  const [tasks, setTask] = useState([
+    'Do laundry',
+    'Go to gym',
+    'Walk dog'
+  ]);
+  
+  const addTask = (task) => {
+    setTask ([...tasks, task])
+  }
+
+  return (
+    <SafeAreaView>
+      <ToDoList tasks={tasks}/>
+      <ToDoForm addTask={addTask}/>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  task: {
+    padding: 10,
+    borderBottomWidth: 1,
+    borderColor: '#ccc',
+  },
+  completed: {
+    backgroundColor: '#e0e0e0',
+  },
+  taskText: {
+    fontSize: 16,
+  },
+  form: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    marginTop: 20,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginRight: 10,
+  },
+});
+
+export default App;
